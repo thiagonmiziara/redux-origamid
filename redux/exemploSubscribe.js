9a321f4096840f7ff131e20f6ff7e95dbea264a0
thiagonmiziara <thiagonmiziara@gmail.com>
2021-02-09 19:56:02 -0300

    add unsubscribe

--- redux/exemploSubscribe.js
+++ redux/exemploSubscribe.js
@@ -1,32 +1,49 @@
-const button = document.querySelector('#button');
-button.addEventListener('click', () => store.dispatch(incrementar()));
+const button = document.querySelector("#button");
+button.addEventListener("click", () => {
+    store.dispatch(incrementar());
+    //para verificar se esta incremetando e so no console do navegador
+    // chamar store.getState();
+});
 
+//Para evitar bugs sempre colocar o Type em constantes
+const INCREMENTAR = "INCREMENTAR";
+const SOMAR = "SOMAR";
 
-function reducer(state = 0, action) {
-    if (action.type === 'incrementar') {
+const initialState = 0;
+
+// Exemplos de action creator
+
+function incrementar() {
+    return { type: INCREMENTAR };
+}
+
+function somar() {
+    return { type: SOMAR, payload: 25 };
+}
+
+/////////////////////////////////////////////////////
+function reducer(state = initialState, action) {
+    //exemplo sem payload
+    if (action.type === INCREMENTAR) {
         return state + 1;
     }
-    if (action.type === 'reduzir') {
-        return state - 1;
+    // exemplo com payload
+    if (action.type === SOMAR) {
+        return state + action.payload;
     }
     return state;
 }
+
 const store = Redux.createStore(reducer);
 
 function render() {
-    const total = document.getElementById('total');
-    total.innerText = store.getState();
+    const state = store.getState();
+
+    const total = document.getElementById("total");
+    total.innerText = state;
 }
-// Ativa a função render sempre que o dispatch ocorrer
-store.subscribe(render);
+render();
 
-store.dispatch({ type: 'incrementar' });
-store.dispatch({ type: 'incrementar' });
-store.dispatch({ type: 'incrementar' });
-store.dispatch({ type: 'incrementar' });
-store.dispatch({ type: 'reduzir' });
-
-store.subscribe(render);
-store.subscribe(() => {
-    console.log('Atualizado');
-});
+const unsubscribe = store.subscribe(render);
+store.dispatch(somar());
+unsubscribe() //não deixa atualizar a pagina
