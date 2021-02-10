73f3e7376ce1b3debc70e49bb5b8b7efd3a946a7
thiagonmiziara <thiagonmiziara@gmail.com>
2021-02-10 17:54:27 -0300

    aprendendo combineReducers

--- redux/exemploSubscribe.js
+++ redux/exemploSubscribe.js
@@ -5,6 +5,19 @@ button.addEventListener("click", () => {
     // chamar store.getState();
 });
 
+const abrir = document.querySelector("#abrir");
+const fechar = document.querySelector("#fechar");
+
+abrir.addEventListener('click', () => {
+    store.dispatch({ type: 'ABRIR' })
+})
+
+fechar.addEventListener('click', () => {
+    store.dispatch({ type: 'FECHAR' })
+})
+
+
+
 //Para evitar bugs sempre colocar o Type em constantes
 const INCREMENTAR = "INCREMENTAR";
 const SOMAR = "SOMAR";
@@ -22,17 +35,40 @@ function somar() {
 }
 
 /////////////////////////////////////////////////////
-function reducer(state = initialState, action) {
-    //exemplo sem payload
-    if (action.type === INCREMENTAR) {
-        return state + 1;
+function contador(state = initialState, action) {
+    /* //exemplo sem payload
+       if (action.type === INCREMENTAR) {
+           return state + 1;
+       }
+       // exemplo com payload
+       if (action.type === SOMAR) {
+           return state + action.payload;
+       }*/
+
+    //exemplo com switch case
+    switch (action.type) {
+        case INCREMENTAR:
+            return state + 1;
+        case SOMAR:
+            return state + action.payload;
+        default:
+            return state;
     }
-    // exemplo com payload
-    if (action.type === SOMAR) {
-        return state + action.payload;
-    }
-    return state;
 }
+
+function modal(state = false, action) {
+
+    switch (action.type) {
+        case "ABRIR":
+            return true;
+        case "FECHAR":
+            return false
+        default:
+            return state;
+    }
+}
+
+const reducer = Redux.combineReducers({ contador, modal })
 
 const store = Redux.createStore(reducer);
 
@@ -40,10 +76,16 @@ function render() {
     const state = store.getState();
 
     const total = document.getElementById("total");
-    total.innerText = state;
+    total.innerText = state.contador;
+    if (state.modal) {
+        total.style.display = 'inline-block'
+    } else {
+        { total.style.display = 'none' }
+    }
 }
 render();
 
 const unsubscribe = store.subscribe(render);
+store.subscribe(render);
 store.dispatch(somar());
-unsubscribe() //não deixa atualizar a pagina
+unsubscribe(); //não deixa atualizar a pagina
