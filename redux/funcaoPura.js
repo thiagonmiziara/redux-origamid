120639b9835ddfc4d8000fd4c4e0f9de26027d81
thiagonmiziara <thiagonmiziara@gmail.com>
2021-02-11 18:00:42 -0300

    entendendo o efeito colateral

---
+++ redux/funcaoPura.js
@@ -0,0 +1,23 @@
+function reducer(state = 0, action) {
+    switch (action.type) {
+        case 'MODIFICAR_WIDTH':
+            return action.payload;
+        default:
+            return state;
+    }
+}
+
+const store = Redux.createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+
+function render() {
+    const caixa = document.getElementById('caixa')
+    caixa.style.width = store.getState() + 'px';
+}
+store.subscribe(render);
+store.dispatch({ type: 'MODIFICAR_WIDTH', payload: 100 });
+
+console.log(store.getState());
