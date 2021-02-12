70b6718c4e5affe9145429bdfc7e8b7ad3921833
thiagonmiziara <thiagonmiziara@gmail.com>
2021-02-12 18:10:24 -0300

    imutabilidade de array e objetos

---
+++ redux/imutabilidade.js
@@ -0,0 +1,37 @@
+const array1 = [0, 1, 2, 3, 4]
+const array2 = [...array1]
+
+const obj1 = {
+    time: 'flamengo',
+    apelido: 'mengão'
+}
+const obj2 = Object.assign({}, obj1, { time: 'Campeão' });
+console.log(obj1);
+console.log(obj2);
+
+
+
+
+
+const initialState = {
+    nome: "Thiago",
+    idade: 33
+}
+
+
+function reducer(state = initialState, action) {
+    switch (action.type) {
+        case 'MUDAR_NOME':
+            state.nome = action.payload;
+            return {...state, nome: action.payload };
+        default:
+            return state;
+    }
+}
+
+const store = Redux.createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+store.dispatch({ type: 'MUDAR_NOME', payload: "Flamengo" });
